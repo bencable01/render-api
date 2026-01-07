228f7177cff3f7b5bb7fa95daa78117d77d2e90f
bencable01 <bcable.development@gmail.com>
2026-01-06 22:57:13 -0500

    Test API working

---
+++ db.js
@@ -0,0 +1,16 @@
+import sqlite3 from 'sqlite3';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const dbPath = path.join(__dirname, 'btdb.db');
+
+export const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error('Failed to connect to SQLite', err);
+  } else {
+    console.log('Connected to SQLite');
+  }
+});
--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 import express from 'express';
+import { db } from './db.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+/* ----------- CORS ----------- */
+app.use(cors({
+  origin: 'http://localhost:4200'
+}));
+
 /* ---------------- API ---------------- */
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello 55 from Node.js on Render 🚀' });
+});
+
+app.get('/api/users', (req, res) => {
+  db.all('select username from test_users where id = 2', [], (err, rows) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json(rows[0]['username']);
+  });
 });
 
 /* ---------------- START ---------------- */
